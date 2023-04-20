a76bfcd9bd16354dbda7728d32f8528592dcfa85
root <root@GOODWALK>
2023-04-20 18:15:54 +0800

    first commit

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c-mode
+  . ((flycheck-clang-include-path
+      . ("/usr/include"))
+     (flycheck-gcc-include-path
+      . ("/usr/include")))))
---
+++ lib/disassembler.c
@@ -0,0 +1,6 @@
+#include <stdint>
+
+int DisassemblerChip8(uint8_t *codebuffer, int pc)
+{
+  uint8_t *code = &codebuffer[pc];
+}
