7974b1d1bef3025eab25d7f50e126a4669b4ff24
root <root@GOODWALK>
2023-04-25 14:16:29 +0800

    added configure.ac, removed .dir-locals.el in favour of Makefile generating compilation tables

--- .dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c-mode
-  . ((flycheck-clang-include-path
-      . ("/usr/include"))
-     (flycheck-gcc-include-path
-      . ("/usr/include")))))
